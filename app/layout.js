eae69183b3daa75b878133def714eec3ff71b3de
alireza-ghaffar <alirezaghaffar.dev@gmail.com>
2023-10-30 09:22:44 -0400

    navbar v1.0.0 done

--- app/layout.js
+++ app/layout.js
@@ -13,17 +13,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 border-b border-black-200">
-        <div className="max-w-5xl mx-auto px-4">
-          <div className="flex items-center justify-between h-14">
+          <div className="flex items-center justify-between px-11 h-16">
             <span className="text-2xl text-gray-900 font-semibold">𝓪𝓵𝓲𝓻𝓮𝔃𝓪 𝓰𝓱𝓪𝓯𝓯𝓪𝓻.𝓭𝓮𝓿</span>
-            <div className="flex space-x-4 text-gray-900">
+            <div className="flex space-x-4 text-gray-900 font-semibold">
               <a href="#Home">Home</a>
               <a href="#About">About</a>
               <a href="#">Projects</a>
               <a href="#">Contact</a>
             </div>
           </div>
-        </div>
       </nav>
     {children}
     </body>
