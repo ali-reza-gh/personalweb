8872c232ac847ea6e09a8399e7efc896103e6ba6
alireza-ghaffar <alirezaghaffar.dev@gmail.com>
2023-10-31 04:28:18 -0400

    handel some inspect error fix som bugs

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
       <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 border-b border-black-200 shadow-md">
           <div className="flex items-center justify-between px-11 h-20">
-            <span className="text-2xl text-gray-900 font-semibold">𝓪𝓵𝓲𝓻𝓮𝔃𝓪 𝓰𝓱𝓪𝓯𝓯𝓪𝓻.𝓭𝓮𝓿</span>
+            <span className="text-2xl text-gray-900 font-semibold">Alireza Ghaffar.dev</span>
             <div className="flex space-x-4 text-gray-900 font-semibold">
               <a href="#Home">Home</a>
               <a href="#About">About</a>
